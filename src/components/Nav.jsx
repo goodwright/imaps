80b0e02a135edf48ded7b6abc843e3a900182502
Sam Ireland <sam.ireland.uk@gmail.com>
2020-10-28 11:40:57 +0000

    Logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Logo from "./Logo";
 
 const Nav = props => {
   return (
     <nav>
-      <div className="logo">imaps</div>
+      <div className="logo"><Logo /></div>
       <div className="nav-main">
         <input />
         <div className="auth-links">
