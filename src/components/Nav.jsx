febe521877ddd0b6547ba12da1d3984e8ba4d130
Sam Ireland <sam.ireland.uk@gmail.com>
2020-10-28 11:55:45 +0000

    Nav search style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import Logo from "./Logo";
+import searchIcon from "../images/searchIcon.svg"
 
 const Nav = props => {
+
+  const [searchText, setSearchText] = useState("");
+
   return (
     <nav>
       <div className="logo"><Logo /></div>
       <div className="nav-main">
-        <input />
+        <div className="input-icon">
+          <img src={searchIcon} className="icon" />
+          <input
+            className="search"
+            value={searchText}
+            onChange={e => setSearchText(e.target.value)}
+            placeholder="Search"
+          />
+        </div>
         <div className="auth-links">
           <button>Log In</button>
           <button>Sign Up</button>
