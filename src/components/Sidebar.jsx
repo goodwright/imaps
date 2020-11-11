69ee4b8f6833419cdea8417774ead37612036aa7
Sam Ireland <sam.ireland.uk@gmail.com>
2020-11-11 01:34:25 +0000

    Mobile GW section

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import UserSummary from "./UserSummary";
 import goodwright from "../images/goodwright.svg";
+import goodwrightIcon from "../images/goodwright-icon.svg";
 
 const Sidebar = props => {
   return (
     <div className="sidebar">
       <UserSummary />
       <div className="goodwright">
-        <a href="https://goodwright.org"><img src={goodwright} alt="goodwright" /></a>
+        <a href="https://goodwright.org">
+          <img src={goodwright} alt="goodwright" className="goodwright-logo"/>
+          <img src={goodwrightIcon} alt="goodwright-icon" className="goodwright-icon" />
+        </a>
         <div className="links">
-          <Link to="/privacy/">Privacy Policy</Link>
-          <Link to="/terms/">Terms of Use</Link>
+          <Link to="/privacy/">Privacy<span className="full"> Policy</span></Link>
+          <Link to="/terms/">Terms<span className="full"> of Use</span></Link>
         </div>
       </div>
     </div>
