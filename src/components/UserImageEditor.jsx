9064b228fffd3557868c202bdc5568be3ac37282
Sam Ireland <sam.ireland.uk@gmail.com>
2020-12-13 11:04:21 +0000

    Better image upload layout

--- src/components/UserImageEditor.jsx
+++ src/components/UserImageEditor.jsx
@@ -47,30 +47,33 @@ const UserImageEditor = props => {
   }
 
   return (
-    <form className="user-image-editor" onSubmit={formSubmit}>
-      <div className="file-upload">
-        <label htmlFor="file" />
-        <input type="file" id="file" ref={fileRef} onChange={imageChanged} accept="image/*"/>
+    <div className="user-image-editor">
+      <div className="title">Edit Photo</div>
+      <form onSubmit={formSubmit}>
+        <div className="file-upload">
+          <label htmlFor="file" />
+          <input type="file" id="file" ref={fileRef} onChange={imageChanged} accept="image/*"/>
 
-        {user.image && !(fileRef.current&& fileRef.current.files.length) ? (
-          <img src={`${getMediaLocation()}${user.image}`} alt=""/>
-        ) : fileRef.current && fileRef.current.files.length ? (
-          <img src={URL.createObjectURL(fileRef.current.files[0])} alt="" />
-        ): (
-          <svg viewBox="0 0 14 14" fill="none">
-            <path d="M6.98437 0.864746L6.98437 13.2715" strokeWidth="1.5"/>
-            <path d="M13.1885 6.90917L0.78176 6.90918" strokeWidth="1.5"/>
-          </svg>
-        )}
-      </div>
-      
-      <div className="error-container">
-        {errors.image && <div className="error">{errors.image}</div> }
-        <button type="submit" className="button primary-button">
-          {updateImageMutation.loading ? <ClipLoader color="white" size="20px" /> : "Upload New Photo"}
-        </button>
-      </div>
-    </form>
+          {user.image && !(fileRef.current&& fileRef.current.files.length) ? (
+            <img src={`${getMediaLocation()}${user.image}`} alt=""/>
+          ) : fileRef.current && fileRef.current.files.length ? (
+            <img src={URL.createObjectURL(fileRef.current.files[0])} alt="" />
+          ): (
+            <svg viewBox="0 0 14 14" fill="none">
+              <path d="M6.98437 0.864746L6.98437 13.2715" strokeWidth="1.5"/>
+              <path d="M13.1885 6.90917L0.78176 6.90918" strokeWidth="1.5"/>
+            </svg>
+          )}
+        </div>
+        
+        <div className="error-container">
+          {errors.image && <div className="error">{errors.image}</div> }
+          <button type="submit" className="button primary-button">
+            {updateImageMutation.loading ? <ClipLoader color="white" size="20px" /> : "Upload New Photo"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
