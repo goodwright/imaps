8e569572f93a8144429b1a8b99286f3e3f4e4f69
Sam Ireland <sam.ireland.uk@gmail.com>
2021-05-20 23:44:10 +0100

    Specific input types for basic fields

--- src/pages/CommandPage.jsx
+++ src/pages/CommandPage.jsx
@@ -3,6 +3,7 @@ import { useRouteMatch } from "react-router";
 import { useQuery } from "@apollo/client";
 import useDocumentTitle from "@rehooks/document-title";
 import ReactMarkdown from "react-markdown";
+import Toggle from "react-toggle";
 import { COMMAND } from "../queries";
 import { detect404 } from "../forms";
 import PageNotFound from "./PageNotFound";
@@ -30,6 +31,7 @@ const CommandPage = () => {
   const command = data.command;
 
   const inputs = JSON.parse(command.inputSchema);
+  console.log(inputs)
 
   return (
     <Base className="command-page">
@@ -39,17 +41,52 @@ const CommandPage = () => {
 
       <form>
         <div className="inputs">
-          {inputs.map(input => (
-            <div className="input" key={input.name}>
-              <label htmlFor={input.name}>{input.name}</label>
-              <div className="label">{input.label}</div>
-              <input
-                required={input.required}
-                value={inputValues[input.name]}
-                onChange={e => setInputValues({...inputValues, [input.name]: e.target.value})}
-              />
-            </div>
-          ))}
+          {inputs.map(input => {
+            if (!input.type) {
+              return (
+                <div className="input" key={input.name}>
+                  <label htmlFor={input.name}>{input.name}</label>
+                  <div className="label">{input.label}</div>
+                  <div>Not yet implemented</div>
+                </div>
+              )
+            }
+            if (input.type.includes("basic:boolean")) {
+              return (
+                <div className="input" key={input.name}>
+                  <label htmlFor={input.name}>{input.name}</label>
+                  <div className="label">{input.label}</div>
+                  <div className="binary-toggle">
+                    <label>false</label>
+                    <Toggle
+                      checked={inputValues[input.name]}
+                      onChange={() => setInputValues({...inputValues, [input.name]: !inputValues[input.name]})}
+                      icons={false}
+                    />
+                    <label>true</label>
+                  </div>
+                </div>
+              )
+            }
+            const type = (
+              input.type.includes("integer") || input.type.includes("decimal")
+            ) ? "number" : (
+              input.type.includes("basic:file")
+            ) ? "file" : "text";
+            
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <input
+                  required={input.required}
+                  value={inputValues[input.name]}
+                  type={type}
+                  onChange={e => setInputValues({...inputValues, [input.name]: e.target.value})}
+                />
+              </div>
+            )
+          })}
         </div>
 
         <button type="submit" className="primary-button">Run Command</button>
