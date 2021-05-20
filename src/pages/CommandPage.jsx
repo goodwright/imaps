8a0ce8492096e0a3974ffe5f87abd23c2613f87f
Sam Ireland <sam.ireland.uk@gmail.com>
2021-05-20 20:29:17 +0100

    Basic command page inputs interface

--- src/pages/CommandPage.jsx
+++ src/pages/CommandPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouteMatch } from "react-router";
 import { useQuery } from "@apollo/client";
 import useDocumentTitle from "@rehooks/document-title";
@@ -12,23 +12,49 @@ const CommandPage = () => {
 
   const commandId = useRouteMatch("/commands/:id").params.id;
 
+  const [inputValues, setInputValues] = useState(null);
+
   const { loading, data, error } = useQuery(COMMAND, {
-    variables: {id: commandId}
+    variables: {id: commandId},
+    onCompleted: data => setInputValues(JSON.parse(data.command.inputSchema).reduce(
+      (prev, curr) => ({...prev, [curr.name]: (curr.default === undefined ? "" : curr.default)}), {}
+    ))
   });
 
   useDocumentTitle(data ? `iMaps - ${data.command.name}` : "iMaps");
 
   if (detect404(error)) return <PageNotFound />
 
-  if (loading) return <Base className="command-page" loading={true} />
+  if (loading || inputValues === null) return <Base className="command-page" loading={true} />
 
   const command = data.command;
+
+  const inputs = JSON.parse(command.inputSchema);
 
   return (
     <Base className="command-page">
       <h1>{command.name}</h1>
 
       <ReactMarkdown className="description">{command.description}</ReactMarkdown>
+
+      <form>
+        <div className="inputs">
+          {inputs.map(input => (
+            <div className="input" key={input.name}>
+              <label htmlFor={input.name}>{input.name}</label>
+              <div className="label">{input.label}</div>
+              <input
+                required={input.required}
+                value={inputValues[input.name]}
+                onChange={e => setInputValues({...inputValues, [input.name]: e.target.value})}
+              />
+            </div>
+          ))}
+        </div>
+
+        <button type="submit" className="primary-button">Run Command</button>
+      </form>
+      
     </Base>
   );
 };
