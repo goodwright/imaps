917c5c81413281accee47b1f564f97a3599c6320
Sam Ireland <sam.ireland.uk@gmail.com>
2021-05-28 12:29:15 +0100

    Create separate CommandInputs component

---
+++ src/components/CommandInputs.jsx
@@ -0,0 +1,268 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useRouteMatch, useLocation } from "react-router";
+import { useLazyQuery, useMutation, useQuery } from "@apollo/client";
+import useDocumentTitle from "@rehooks/document-title";
+import ReactMarkdown from "react-markdown";
+import Toggle from "react-toggle";
+import Select from "react-select";
+import Creatable from "react-select/creatable";
+import { COMMAND, COLLECTION_DATA, POSSIBLE_EXECUTIONS, COLLECTION } from "../queries";
+import { detect404 } from "../forms";
+import ExecutionHistory from "../components/ExecutionHistory";
+import { RUN_COMMAND } from "../mutations";
+import { BarLoader } from "react-spinners";
+import Paginator from "../components/Paginator";
+
+const CommandInputs = props => {
+
+  const { inputSchema, inputValues, setInputValues, collection } = props;
+  console.log(inputSchema)
+  const possibleExecutions = inputSchema.filter(input => input.type && input.type.includes("data:")).reduce(
+    (prev, curr) => ({...prev, [curr.name]: {
+      name: curr.name, page: 1, executions: null, limitToCollection: true,
+      type: curr.type.slice(curr.type.indexOf("data:") + 5)
+    }}), {}
+  )
+  const [executions, setExecutions] = useState(possibleExecutions);
+  const PER_PAGE = 8;
+
+  // From the current state, do any possible executions need to be loaded?
+  let requiredData = null;
+  if (executions) {
+    const waitingInputs = Object.values(executions).filter(input => input.executions === null);
+    if (waitingInputs.length) {
+      requiredData = [waitingInputs[0].name, waitingInputs[0].type, waitingInputs[0].page, waitingInputs[0].limitToCollection]
+    }
+  }
+
+  // Fetch possible executions if any are needed
+  useQuery(POSSIBLE_EXECUTIONS, {
+    skip: !requiredData,
+    variables: requiredData && {
+      dataType: requiredData[1],
+      collection: requiredData[3] && collection ? collection.id : null,
+      first: PER_PAGE * requiredData[2],
+      last: PER_PAGE
+    },
+    onCompleted: data => {
+      setExecutions({...executions, [requiredData[0]]: {
+        ...executions[requiredData[0]],
+        executions: data.executions
+      }})
+    }
+  })
+
+
+  return (
+    <div className="command-inputs">
+      {inputSchema.map(input => {
+        // Data input?
+        if (input.type && input.type.includes("data:")) {
+          if (!executions[input.name].executions) {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <BarLoader color="#6353C6" css="margin-top: 16px" />
+              </div>
+            )
+          } else {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                {collection && <div className="collection-checkbox">
+                  <input
+                    type="checkbox"
+                    checked={executions[input.name].limitToCollection}
+                    onChange={() => setExecutions({...executions, [input.name]: {
+                      ...executions[input.name],
+                      executions: null,
+                      page: 1,
+                      limitToCollection: !executions[input.name].limitToCollection
+                    }})}
+                  />
+                  <label>Limit options to collection</label>
+                </div>}
+                {executions[input.name].executions.count > PER_PAGE && (
+                  <Paginator
+                    count={executions[input.name].executions.count}
+                    currentPage={executions[input.name].page}
+                    itemsPerPage={PER_PAGE}
+                    onClick={page => setExecutions({...executions, [input.name]: {
+                      ...executions[input.name],
+                      executions: null,
+                      page
+                    }})}
+                  />
+                )}
+                <ExecutionHistory
+                  executions={executions[input.name].executions.edges.map(edge => edge.node)}
+                  selected={inputValues[input.name]}
+                  setSelected={value => setInputValues({...inputValues, [input.name]: value})}
+                />
+                {executions[input.name].executions.count === 0 && (
+                  <div className="no-data">No data of the correct type available.</div>
+                )}
+              </div>
+            )
+          }
+        }
+
+        // Boolean input?
+        if (input.type && input.type === "basic:boolean:") {
+          return (
+            <div className="input" key={input.name}>
+              <label htmlFor={input.name}>{input.name}</label>
+              <div className="label">{input.label}</div>
+              <div className="binary-toggle">
+                <label className={inputValues[input.name] ? "" : "selected"}>false</label>
+                <Toggle
+                  checked={inputValues[input.name]}
+                  onChange={() => setInputValues({...inputValues, [input.name]: !inputValues[input.name]})}
+                  icons={false}
+                  disabled={input.disabled}
+                />
+                <label className={!inputValues[input.name] ? "" : "selected"}>true</label>
+              </div>
+            </div>
+          )
+        }
+
+        // String input?
+        if (input.type && (input.type.includes("string:") || input.type.includes("text:"))) {
+          console.log(input)
+          // List of strings
+          if (input.type.slice(0, 5) === "list:") {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <Creatable
+                  isMulti={true}
+                  value={inputValues[input.name].map(s => ({label: s, value: s}))}
+                  onChange={values => setInputValues({...inputValues, [input.name]: (
+                    values ? values.map(value => value.value) : []
+                  )})}
+                  noOptionsMessage={() => ""}
+                  formatCreateLabel={label => label}
+                  placeholder={input.placeholder || ""}
+                  className="react-select"
+                  classNamePrefix="react-select"
+                />
+              </div>
+            )
+          // Choices, can create
+          } else if (input.choices && input.allow_custom_choice) {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <Creatable
+                  options={input.choices}
+                  onChange={value => setInputValues({...inputValues, [input.name]: (
+                    value.value
+                  )})}
+                  isDisabled={input.disabled}
+                  placeholder={input.placeholder || ""}
+                  className="react-select"
+                  classNamePrefix="react-select"
+                />
+              </div>
+            )
+          // Choices, can't create
+          } else if (input.choices) {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <Select
+                  options={input.choices}
+                  onChange={value => setInputValues({...inputValues, [input.name]: (
+                    value.value
+                  )})}
+                  placeholder={input.placeholder || ""}
+                  isDisabled={input.disabled}
+                  className="react-select"
+                  classNamePrefix="react-select"
+                />
+              </div>
+            )
+          // Free text
+          } else {
+            return (
+              <div className="input" key={input.name}>
+                <label htmlFor={input.name}>{input.name}</label>
+                <div className="label">{input.label}</div>
+                <input
+                  value={inputValues[input.name]}
+                  onChange={e => setInputValues({...inputValues, [input.name]: e.target.value})}
+                  required={input.required}
+                  disabled={input.disabled}
+                />
+              </div>
+            )
+          }
+        }
+
+        // Numeric input?
+        if (input.type && (input.type.includes("integer:") || input.type.includes("decimal:"))) {
+          const parse = input.type.includes("integer:") ? parseInt : parseFloat
+          return (
+            <div className="input" key={input.name}>
+              <label htmlFor={input.name}>{input.name}</label>
+              <div className="label">{input.label}</div>
+              <input
+                type="number"
+                value={inputValues[input.name]}
+                onChange={e => setInputValues({...inputValues, [input.name]: parse(e.target.value)})}
+                min={input.range ? input.range[0] : null}
+                max={input.range ? input.range[1] : null}
+                required={input.required}
+                disabled={input.disabled}
+              />
+            </div>
+          )
+        }
+
+        // File input?
+        if (input.type && input.type.includes("file:")) {
+          const isMulti = input.type.slice(0, 5) === "list:";
+          return (
+            <div className="input" key={input.name}>
+              <label htmlFor={input.name}>{input.name}</label>
+              <div className="label">{input.label}</div>
+              <input
+                type="file"
+                onChange={e => setInputValues({...inputValues, [input.name]: isMulti ? (
+                  [...e.target.files]
+                ) : (
+                  e.target.files.length ? e.target.files[0] : null
+                )})}
+                multiple={isMulti}
+                accept={input.validate_regex || undefined}
+                required={input.required}
+                disabled={input.disabled}
+              />
+            </div>
+          )
+        }
+
+        // Return not implemented
+        return (
+          <div className="input" key={input.name}>
+            <label htmlFor={input.name}>{input.name}</label>
+            <div className="label">{input.label}</div>
+            <div>Not yet implemented</div>
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
+CommandInputs.propTypes = {
+  
+};
+
+export default CommandInputs;
--- src/pages/CommandPage.jsx
+++ src/pages/CommandPage.jsx
@@ -14,6 +14,7 @@ import Base from "./Base";
 import { RUN_COMMAND } from "../mutations";
 import { BarLoader } from "react-spinners";
 import Paginator from "../components/Paginator";
+import CommandInputs from "../components/CommandInputs";
 
 const createDefaults = inputSchema => {
   const inputs = {};
@@ -40,24 +41,12 @@ const CommandPage = () => {
   const commandId = useRouteMatch("/commands/:id").params.id;
 
   const [inputValues, setInputValues] = useState(null);
-  const [executions, setExecutions] = useState(null);
   console.log(inputValues)
-
   const history = useHistory();
-  const PER_PAGE = 8;
 
   // Has a collection been specified?
   const params = new URLSearchParams(useLocation().search);
   const collectionId = params.get("collection");
-
-  // From the current state, do any possible executions need to be loaded?
-  let requiredData = null;
-  if (executions) {
-    const waitingInputs = Object.values(executions).filter(input => input.executions === null);
-    if (waitingInputs.length) {
-      requiredData = [waitingInputs[0].name, waitingInputs[0].type, waitingInputs[0].page, waitingInputs[0].limitToCollection]
-    }
-  }
 
   // Get the command
   const { loading, data, error } = useQuery(COMMAND, {
@@ -67,15 +56,7 @@ const CommandPage = () => {
       const inputSchema = JSON.parse(data.command.inputSchema);
 
       // Create the initial inputs object
-      setInputValues(createDefaults(inputSchema))
-
-      // Create the executions object
-      setExecutions(inputSchema.filter(input => input.type && input.type.includes("data:")).reduce(
-        (prev, curr) => ({...prev, [curr.name]: {
-          name: curr.name, page: 1, executions: null, limitToCollection: true,
-          type: curr.type.slice(curr.type.indexOf("data:") + 5)
-        }}), {}
-      ))      
+      setInputValues(createDefaults(inputSchema))     
     }
   });
 
@@ -83,23 +64,6 @@ const CommandPage = () => {
   const collectionQuery = useQuery(COLLECTION, {
     skip: loading || !collectionId,
     variables: {id: collectionId}
-  })
-
-  // Fetch possible executions if any are needed
-  useQuery(POSSIBLE_EXECUTIONS, {
-    skip: !requiredData,
-    variables: requiredData && {
-      dataType: requiredData[1],
-      collection: requiredData[3] ? collectionId : null,
-      first: PER_PAGE * requiredData[2],
-      last: PER_PAGE
-    },
-    onCompleted: data => {
-      setExecutions({...executions, [requiredData[0]]: {
-        ...executions[requiredData[0]],
-        executions: data.executions
-      }})
-    }
   })
 
   // Update the page title
@@ -116,7 +80,7 @@ const CommandPage = () => {
   const collection = collectionQuery.data && collectionQuery.data.collection;
 
   // Can the form be shown?
-  const canShowForm = inputValues && executions && (!collectionId || collectionQuery.data);
+  const canShowForm = inputValues && (!collectionId || collectionQuery.data);
 
   return (
     <Base className="command-page">
@@ -128,211 +92,14 @@ const CommandPage = () => {
 
       {canShowForm && (
         <form>
-          {JSON.parse(command.inputSchema).map(input => {
-            // Data input?
-            if (input.type && input.type.includes("data:")) {
-              if (!executions[input.name].executions) {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    <BarLoader color="#6353C6" css="margin-top: 16px" />
-                  </div>
-                )
-              } else {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    {collectionId && <div className="collection-checkbox">
-                      <input
-                        type="checkbox"
-                        checked={executions[input.name].limitToCollection}
-                        onChange={() => setExecutions({...executions, [input.name]: {
-                          ...executions[input.name],
-                          executions: null,
-                          page: 1,
-                          limitToCollection: !executions[input.name].limitToCollection
-                        }})}
-                      />
-                      <label>Limit options to collection</label>
-                    </div>}
-                    {executions[input.name].executions.count > PER_PAGE && (
-                      <Paginator
-                        count={executions[input.name].executions.count}
-                        currentPage={executions[input.name].page}
-                        itemsPerPage={PER_PAGE}
-                        onClick={page => setExecutions({...executions, [input.name]: {
-                          ...executions[input.name],
-                          executions: null,
-                          page
-                        }})}
-                      />
-                    )}
-                    <ExecutionHistory
-                      executions={executions[input.name].executions.edges.map(edge => edge.node)}
-                      selected={inputValues[input.name]}
-                      setSelected={value => setInputValues({...inputValues, [input.name]: value})}
-                    />
-                    {executions[input.name].executions.count === 0 && (
-                      <div className="no-data">No data of the correct type available.</div>
-                    )}
-                  </div>
-                )
-              }
-            }
-
-            // Boolean input?
-            if (input.type && input.type === "basic:boolean:") {
-              return (
-                <div className="input" key={input.name}>
-                  <label htmlFor={input.name}>{input.name}</label>
-                  <div className="label">{input.label}</div>
-                  <div className="binary-toggle">
-                    <label className={inputValues[input.name] ? "" : "selected"}>false</label>
-                    <Toggle
-                      checked={inputValues[input.name]}
-                      onChange={() => setInputValues({...inputValues, [input.name]: !inputValues[input.name]})}
-                      icons={false}
-                      disabled={input.disabled}
-                    />
-                    <label className={!inputValues[input.name] ? "" : "selected"}>true</label>
-                  </div>
-                </div>
-              )
-            }
-
-            // String input?
-            if (input.type && (input.type.includes("string:") || input.type.includes("text:"))) {
-              console.log(input)
-              // List of strings
-              if (input.type.slice(0, 5) === "list:") {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    <Creatable
-                      isMulti={true}
-                      value={inputValues[input.name].map(s => ({label: s, value: s}))}
-                      onChange={values => setInputValues({...inputValues, [input.name]: (
-                        values ? values.map(value => value.value) : []
-                      )})}
-                      noOptionsMessage={() => ""}
-                      formatCreateLabel={label => label}
-                      placeholder={input.placeholder || ""}
-                      className="react-select"
-                      classNamePrefix="react-select"
-                    />
-                  </div>
-                )
-              // Choices, can create
-              } else if (input.choices && input.allow_custom_choice) {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    <Creatable
-                      options={input.choices}
-                      onChange={value => setInputValues({...inputValues, [input.name]: (
-                        value.value
-                      )})}
-                      isDisabled={input.disabled}
-                      placeholder={input.placeholder || ""}
-                      className="react-select"
-                      classNamePrefix="react-select"
-                    />
-                  </div>
-                )
-              // Choices, can't create
-              } else if (input.choices) {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    <Select
-                      options={input.choices}
-                      onChange={value => setInputValues({...inputValues, [input.name]: (
-                        value.value
-                      )})}
-                      placeholder={input.placeholder || ""}
-                      isDisabled={input.disabled}
-                      className="react-select"
-                      classNamePrefix="react-select"
-                    />
-                  </div>
-                )
-              // Free text
-              } else {
-                return (
-                  <div className="input" key={input.name}>
-                    <label htmlFor={input.name}>{input.name}</label>
-                    <div className="label">{input.label}</div>
-                    <input
-                      value={inputValues[input.name]}
-                      onChange={e => setInputValues({...inputValues, [input.name]: e.target.value})}
-                      required={input.required}
-                      disabled={input.disabled}
-                    />
-                  </div>
-                )
-              }
-            }
-
-            // Numeric input?
-            if (input.type && (input.type.includes("integer:") || input.type.includes("decimal:"))) {
-              const parse = input.type.includes("integer:") ? parseInt : parseFloat
-              return (
-                <div className="input" key={input.name}>
-                  <label htmlFor={input.name}>{input.name}</label>
-                  <div className="label">{input.label}</div>
-                  <input
-                    type="number"
-                    value={inputValues[input.name]}
-                    onChange={e => setInputValues({...inputValues, [input.name]: parse(e.target.value)})}
-                    min={input.range ? input.range[0] : null}
-                    max={input.range ? input.range[1] : null}
-                    required={input.required}
-                    disabled={input.disabled}
-                  />
-                </div>
-              )
-            }
-
-            // File input?
-            if (input.type && input.type.includes("file:")) {
-              const isMulti = input.type.slice(0, 5) === "list:";
-              return (
-                <div className="input" key={input.name}>
-                  <label htmlFor={input.name}>{input.name}</label>
-                  <div className="label">{input.label}</div>
-                  <input
-                    type="file"
-                    onChange={e => setInputValues({...inputValues, [input.name]: isMulti ? (
-                      [...e.target.files]
-                    ) : (
-                      e.target.files.length ? e.target.files[0] : null
-                    )})}
-                    multiple={isMulti}
-                    accept={input.validate_regex || undefined}
-                    required={input.required}
-                    disabled={input.disabled}
-                  />
-                </div>
-              )
-            }
-
-            // Return not implemented
-            return (
-              <div className="input" key={input.name}>
-                <label htmlFor={input.name}>{input.name}</label>
-                <div className="label">{input.label}</div>
-                <div>Not yet implemented</div>
-              </div>
-            )
-          })}
+          <CommandInputs
+            inputSchema={JSON.parse(command.inputSchema)}
+            inputValues={inputValues}
+            setInputValues={setInputValues}
+            collection={collection}
+          />
         </form>
       )}
-
     </Base>
   )
 };
