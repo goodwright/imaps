73115097973d9fd28889ed1d2183fe73ae37864b
Sam Ireland <sam.ireland.uk@gmail.com>
2021-06-29 18:19:52 +0100

    Refactor Modal for tailwind

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
-import { useRef } from "react";
 
 const Modal = props => {
   /**
    * A white box that floats over the page.
    */
 
-  const { showModal, setShowModal } = props;
+  const { showModal, setShowModal, title, text } = props;
   const boxRef = useRef();
-
-  const className = classNames({modal: true, visible: showModal});
-
-  const boxClassName = classNames({
-    "modal-box": true, "no-scroll": true, [props.className]: true
-  });
 
   const dismiss = e => {
     if (!boxRef.current.contains(e.target)) {
@@ -24,8 +16,10 @@ const Modal = props => {
   }
 
   return (
-    <div className={className} onClick={dismiss}>
-      <div className={boxClassName} ref={boxRef}>
+    <div className={showModal ? "fixed left-0 top-0 w-full h-full flex items-center bg-opacity-40 justify-center bg-gray-700 z-50" : "hidden"} onClick={dismiss}>
+      <div className={`bg-white rounded-md w-max overflow-hidden p-7 p-6 ml-6 mr-6 ${props.className || ""}`} ref={boxRef}>
+        {title && <div className="text-primary-500 text-3xl mb-3">{title}</div>}
+        {text && <div className="text-primary-700 mb-4">{text}</div>}
         {props.children}
       </div>
     </div>
@@ -33,9 +27,14 @@ const Modal = props => {
 };
 
 Modal.propTypes = {
-  className: PropTypes.string.isRequired,
   showModal: PropTypes.bool.isRequired,
-  setShowModal: PropTypes.func.isRequired
+  setShowModal: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  text: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
 };
 
 export default Modal;
